02e9bfda38532c8bbe340bb73209d69cf20e0a0b
failbreak <69219123+failbreak@users.noreply.github.com>
2020-09-21 22:49:00 +0200

    Update Clusterfuck.cs

--- Clusterfuck.cs
+++ Clusterfuck.cs
@@ -7,65 +7,59 @@ using System.Threading.Tasks;
 
 namespace SmileyRapport_Csharp
 {
-    public class ClusterFuck
+     public class ClusterFuck
     {
         string[] TilfredsHedsSkala = new string[4] { "Super Tilfreds", "Tilfreds", "ikke Tilfreds", "Virklig Utilfreds" };
-        List<Tuple<DateTime, int>> Ratings = new List<Tuple<DateTime, int>>();
-        public ClusterFuck()
+        List<DateTime> Ratings = new List<DateTime>();
+        string name; 
+        public ClusterFuck(string name)
         {
-            if (!File.Exists("TilfredsHedsData.txt"))
+            this.name = name;
+            if (!File.Exists(name + ".txt"))
             {
-                File.Create("TilfredsHedsData.txt").Close();
+                File.Create(name + ".txt").Close();
                 
             }
-            using (StreamReader sr = new StreamReader("TilfredsHedsData.txt"))
+            using (StreamReader sr = new StreamReader(name + ".txt"))
             {
                 string line;
                 // Read and display lines from the file until the end of
                 // the file is reached.
                 while ((line = sr.ReadLine()) != null)
                 {
-
-                    string[] teach = line.Split('-');
-                    DateTime allanErBoomer = DateTime.Parse(teach[0]);
-                    int RateShadowLegends = Array.IndexOf(TilfredsHedsSkala, teach[1]);
-                    Ratings.Add(new Tuple<DateTime, int>(allanErBoomer, RateShadowLegends));
+                    DateTime allanErBoomer = DateTime.Parse(line);
+                    Ratings.Add(allanErBoomer);
                 }
             }
 
         }
-        public void ToSaveThemIsMyCause()
+        public void ToSaving()
         {
-            using (StreamWriter sr = new StreamWriter("TilfredsHedsData.txt"))
+            using (StreamWriter sr = new StreamWriter(name + ".txt"))
             {
                 foreach (var Tilfredshed in Ratings)
                 {
-                    sr.WriteLine(Tilfredshed.Item1 + "-" + TilfredsHedsSkala[Tilfredshed.Item2]);
+                    sr.WriteLine(Tilfredshed);
                 }
 
             }
         }
-        public void RatingOthersBeHating(int RatingTheKristneBøger)
+        public void Rating()
         {
-            Ratings.Add(new Tuple<DateTime, int>(DateTime.Now, RatingTheKristneBøger));
-            while (Ratings.Count >= 100)
-            {
-                Ratings.RemoveAt(0);
-            }
-            ToSaveThemIsMyCause();
+            Ratings.Add(DateTime.Now);
+           
+            ToSaving();
         }
-        public void InterspeciesValudering()
+        public int Valudering()
         {
-            List<int> wut = Ratings.Select(x => x.Item2).ToList(); //transformere hver enkel item fra listen //NILLER SAID SO//
-            float TotallyNotNillerSuperTilfreds = wut.FindAll(x => x == 0).Count();
-            float TotallyNoNillerTilfreds = wut.FindAll(x => x == 1).Count();
-            float TotallyNoNillerIkkeTilfreds = wut.FindAll(x => x == 2).Count();
-            float TotallyNoNillerVirkligUtilfreds = wut.FindAll(x => x == 3).Count();
-            float EtNavn = wut.Count();
-            Console.WriteLine($"SuperTilfreds {TotallyNotNillerSuperTilfreds / EtNavn}");
-            Console.WriteLine($"Tilfreds {TotallyNoNillerTilfreds / EtNavn}");
-            Console.WriteLine($"IkkeTilfreds {TotallyNoNillerIkkeTilfreds / EtNavn}");
-            Console.WriteLine($"VirkligUtilfreds {TotallyNoNillerVirkligUtilfreds / EtNavn}");
+            return this.Ratings.Count;
+        }
+
+        public void NoCredit()
+        {
+            this.Ratings = new List<DateTime>();
+            ToSaving();
+
         }
     }
 }
